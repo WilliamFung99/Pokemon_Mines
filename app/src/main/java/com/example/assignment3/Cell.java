60337f45bfae25cd4c2c416398a1dcdbae3a91c7
Kenneth <jll26@sfu.ca>
2020-02-14 19:15:48 -0800

    created a cell class

---
+++ app/src/main/java/com/example/assignment3/Cell.java
@@ -0,0 +1,16 @@
+package com.example.assignment3;
+
+/*
+*
+* Stores row and column / x and y information about a cell in the matrix
+*
+* */
+
+
+
+public class Cell {
+
+    private int x;
+    private int y;
+
+}
