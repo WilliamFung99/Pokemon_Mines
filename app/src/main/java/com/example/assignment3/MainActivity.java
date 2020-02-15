8d1fbfe6e4dc19ca35b569498999eb731ff6698c
william <wfa15@sfu.ca>
2020-02-15 00:07:26 -0800

    Add basic menu

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.assignment3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent mainMenu = MainMenu.makeIntent(MainActivity.this);
+                startActivity(mainMenu);
             }
         });
     }
