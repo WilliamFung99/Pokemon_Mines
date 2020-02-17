478e9e2d2d9ac46afb494d1431397f3008fea48a
Kenneth <jll26@sfu.ca>
2020-02-16 18:08:11 -0800

    wired up help screen

---
+++ app/src/main/java/com/example/assignment3/Help.java
@@ -0,0 +1,29 @@
+package com.example.assignment3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public static Intent makeIntentForHelp(Context context){
+        return new Intent(context, Help.class);
+    }
+
+}
--- app/src/main/java/com/example/assignment3/MainMenu.java
+++ app/src/main/java/com/example/assignment3/MainMenu.java
@@ -19,6 +19,7 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         setUpOptionsBtn();
+        setUpHelpBtn();
     }
 
     private void setUpOptionsBtn(){
@@ -27,6 +28,17 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = Options.makeIntentForOptions(MainMenu.this);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void setUpHelpBtn(){
+        Button helpButton = (Button) findViewById(R.id.btnHelp);
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Help.makeIntentForHelp(MainMenu.this);
                 startActivity(intent);
             }
         });
