65097c3770578348e13becd48f61ed093dce285a
william <wfa15@sfu.ca>
2020-02-18 23:55:58 -0800

    Pressing back on the main menu screen exits the game

--- app/src/main/java/com/example/assignment3/MainMenu.java
+++ app/src/main/java/com/example/assignment3/MainMenu.java
@@ -42,7 +42,13 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+    }
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 
