d75563209a978da4a55420a0968792ea55e5ae86
Kenneth <jll26@sfu.ca>
2020-02-18 20:37:21 -0800

    wired up congrats pop up

--- app/src/main/java/com/example/assignment3/Help.java
+++ app/src/main/java/com/example/assignment3/Help.java
@@ -1,7 +1,10 @@
 package com.example.assignment3;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,12 +14,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Help extends AppCompatActivity {
 
     TextView homePageLink;
+    Dialog dialog;
+    Button congratsPopup;
+    Button ok;
+    ImageView close;
 
 
     @Override
@@ -26,6 +36,36 @@ public class Help extends AppCompatActivity {
 
         homePageLink = (TextView)findViewById(R.id.hyperlinkTextView);
         homePageLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        dialog = new Dialog(this);
+
+        congratsPopup = (Button) findViewById(R.id.btncongrats);
+
+        congratsPopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCongratsPopup();
+            }
+        });
+
+
+    }
+
+    private void showCongratsPopup() {
+        dialog.setContentView(R.layout.congrats_popup);
+        close = (ImageView) dialog.findViewById(R.id.close);
+        ok = (Button) findViewById(R.id.btnOk);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        
+        dialog.show();
 
     }
 
